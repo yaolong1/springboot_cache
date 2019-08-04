0506b709c67bd65e40934784ab5a835f8d688901
“yaolong <1638538651@qq.com>
2019-08-04 13:33:44 +0800

    springboot_cache2

--- springboot_cache/src/main/java/com/example/springboot_cache/SpringbootCacheApplication.java
+++ springboot_cache/src/main/java/com/example/springboot_cache/SpringbootCacheApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * 快速体验缓存
@@ -17,6 +18,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @MapperScan("com.example.springboot_cache.mapper")
 @EnableCaching
+@EnableTransactionManagement
 public class SpringbootCacheApplication {
 
     public static void main(String[] args) {
